172eee0fcdaff3bccbb4066f8861752e27aa4d8e
gleydson-delcho <gleydson.engenhariacivil@gmail.com>
2020-10-14 14:01:11 -0300

    Adiciona a pasta backend todos os arquivos necessários para o funcionamento do servidor

---
+++ backend/README.md
@@ -0,0 +1,14 @@
+## Start backend use npm
+<p>npm start<p>
+
+
+## run migrations
+
+### create migrations
+<p>tyoeorm migration:create</p>
+
+### run migrations
+<p>npm run migration:run</p>
+
+### revert migrations
+<p>npm run migration:revert</p>
---
+++ backend/src/server.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+import 'express-async-errors';
+
+import './database/connection';
+
+import errorHandler from './errors/handler';
+
+import routes from './routes';
+
+const app = express();
+const port = 3333;
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+app.use(errorHandler);
+
+app.listen(port, () => {
+    console.log("servidor rodando na porta 3333.")
+});
+
+
+    
